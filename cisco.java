4cea07128257297e8880d9cb5e08fed6c4eed69f
phyoKhine18 <pkhine3@mail.ccsf.edu>
2016-06-24 15:06:57 -0700

    I added something. CHECK IT OUT

--- cisco.java
+++ cisco.java
@@ -1,5 +1,6 @@
 public class cisco {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("I am from San Francisco.");
 	}
 }
--- sanfran.java
+++
@@ -1,6 +0,0 @@
-public class sanfran{
-	public static void main(String[] args) {
-		int n = 10;
-		System.out.println(n * 100);
-	}
-}
