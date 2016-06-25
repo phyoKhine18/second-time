88bf23981a78197e0a80edc096eec77b66b1f82b
phyoKhine18 <pkhine3@mail.ccsf.edu>
2016-06-24 23:49:56 -0700

    Unfortunately,...

--- Phyo.java/src/phyo1.java
+++ Phyo.java/src/phyo1.java
@@ -4,6 +4,8 @@ public class phyo1 {
 	public static void main(String[] args) {
 		System.out.println("It is hot today.");
 		System.out.println("I could go for a milk tea.");
+		
+		System.out.println("Milk tea is too sweet, unfortunately.");
 	}
 
 }
